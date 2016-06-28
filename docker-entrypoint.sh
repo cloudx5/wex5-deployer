ea230715356a1ec3b79ad50403b48005c3755612
wsh <wsh@justep.com>
2016-06-28 20:09:35 +0800

    数据库放到最后初始化，缩缩短服务检查间隔

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -78,67 +78,7 @@ else
 fi
 echo "更新资源完毕"
 
-# init database
-if [ "$INIT_DB"x = "false"x ]; then
-  echo '$INIT_DB=false，忽略数据库初始化'
-else
-  SQL_PATH="$JUSTEP_HOME/sql"
-  mkdir -p $SQL_PATH
-  LOG_PATH="$SQL_PATH/sql_`date +%Y%m%d%H%M%S`.log"
-  load_script(){
-    TMP="tmp_script"
-    echo "DROP DATABASE IF EXISTS x5;" >>$TMP
-    echo "CREATE DATABASE x5;" >>$TMP
-    echo "USE x5;" >>$TMP
-    echo "SET FOREIGN_KEY_CHECKS=0;" >>$TMP
-    echo "SET SQL_MODE='NO_AUTO_VALUE_ON_ZERO';" >>$TMP
-    SQL_FILES=`ls -A $1/*.sql`
-    if [ "$SQL_FILES" ]; then
-      for FILE_NAME in $SQL_FILES;do
-        echo "source $FILE_NAME;" >>$TMP
-      done
-    fi
-    echo "SET FOREIGN_KEY_CHECKS=1;" >>$TMP
-    echo "SET SQL_MODE='STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION';" >>$TMP
-    echo "commit;" >>$TMP
-    echo "quit" >>$TMP
-    echo "" >>$TMP
-
-    testsql=( ./mysql -hdatabase -uroot -px5 )
-    for i in {3..0}; do
-      if echo 'SELECT 1' | ${testsql[@]} &> /dev/null; then
-        break
-      fi
-      echo '  连接数据库失败，10秒后重试...'
-      sleep 10 
-    done
-
-    if [ "$i" = 0 ]; then
-      error '  数据库连接失败，请检查部署环境' 1
-    fi
-
-    START_TIME=$(date "+%s")
-    ./mysql --default-character-set=utf8 -hdatabase -uroot -px5 -ve "source $TMP" >$LOG_PATH 2>&1
-    ERROR=$?
-    if [ "$ERROR" -eq "0" ]; then
-      echo "  数据库初始化成功！共计用时: " `expr $(date "+%s") - ${START_TIME}` " 秒"
-    else
-      head $LOG_PATH
-      error "  [$ERROR]数据库初始化失败" 1
-    fi
-  }
-
-  echo "开始数据库初始化..."
-  mkdir -p $SQL_PATH
-  cd $SQL_PATH
-  echo "  获取mysql客户端..."
-  curl -s -f $PRODUCT_URL/mysql/5.6/mysql -o mysql
-  chmod a+x mysql
-  load_script $SQL_PATH
-  echo "数据库初始化完毕"
-fi
-
-# webapps最后在更新，避免相关资源未准备而访问错误
+# webapps资源更新后再更新，避免相关资源未准备而访问错误
 
 download_webapps(){
   rm -rf $WEBAPPS_DIR/webapps.txt
@@ -185,5 +125,62 @@ echo "正在更新自定义webapps..."
 download_webapps $DIST_URL/webapps
 echo "自定义webapps更新完毕"
 
-echo "****FINISHED****"
+# 数据库初始化，由于数据库容器启动慢，放到最后执行
+if [ "$INIT_DB"x = "false"x ]; then
+  echo '$INIT_DB=false，忽略数据库初始化'
+else
+  SQL_PATH="$JUSTEP_HOME/sql"
+  mkdir -p $SQL_PATH
+  LOG_PATH="$SQL_PATH/sql_`date +%Y%m%d%H%M%S`.log"
+  load_script(){
+    TMP="tmp_script"
+    echo "DROP DATABASE IF EXISTS x5;" >>$TMP
+    echo "CREATE DATABASE x5;" >>$TMP
+    echo "USE x5;" >>$TMP
+    echo "SET FOREIGN_KEY_CHECKS=0;" >>$TMP
+    echo "SET SQL_MODE='NO_AUTO_VALUE_ON_ZERO';" >>$TMP
+    SQL_FILES=`ls -A $1/*.sql 2> /dev/null`
+    for FILE_NAME in $SQL_FILES;do
+      echo "source $FILE_NAME;" >>$TMP
+    done
+    echo "SET FOREIGN_KEY_CHECKS=1;" >>$TMP
+    echo "SET SQL_MODE='STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION';" >>$TMP
+    echo "commit;" >>$TMP
+    echo "quit" >>$TMP
+    echo "" >>$TMP
 
+    testsql=( ./mysql -hdatabase -uroot -px5 )
+    for i in {9..0}; do
+      if echo 'SELECT 1' | ${testsql[@]} &> /dev/null; then
+        break
+      fi
+      echo '  连接数据库失败，5秒后重试...'
+      sleep 5 
+    done
+
+    if [ "$i" = 0 ]; then
+      error '  数据库连接失败，请检查部署环境' 1
+    fi
+
+    START_TIME=$(date "+%s")
+    ./mysql --default-character-set=utf8 -hdatabase -uroot -px5 -ve "source $TMP" >$LOG_PATH 2>&1
+    ERROR=$?
+    if [ "$ERROR" -eq "0" ]; then
+      echo "  数据库初始化成功！共计用时: " `expr $(date "+%s") - ${START_TIME}` " 秒"
+    else
+      head $LOG_PATH
+      error "  [$ERROR]数据库初始化失败" 1
+    fi
+  }
+
+  echo "开始数据库初始化..."
+  mkdir -p $SQL_PATH
+  cd $SQL_PATH
+  echo "  获取mysql客户端..."
+  curl -s -f $PRODUCT_URL/mysql/5.6/mysql -o mysql
+  chmod a+x mysql
+  load_script $SQL_PATH
+  echo "数据库初始化完毕"
+fi
+
+echo "****FINISHED****"
