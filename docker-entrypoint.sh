7e7b13cb8798f03b566f21104945d2d73130a480
VFT <tiger435@sina.com>
2016-12-08 13:46:55 +0800

    update agent

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -9,5 +9,5 @@ if [ -z "$POOL_TYPE" ]; then
 else
   echo "****FINISHED****"
   echo "启动池类型为$POOL_TYPE的监听"
-  java -jar /usr/local/agent/agent-1.0.1.jar $POOL_TYPE
+  java -Xmx64m -jar /usr/local/agent/agent-1.0.1.jar $POOL_TYPE
 fi
