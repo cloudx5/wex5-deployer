cae5b74ec5a0e7837158c50a649b553714c75fb8
wyt <tnt.w@qq.com>
2016-12-23 09:13:23 +0800

    兼容以前的模式

--- common.sh
+++ common.sh
@@ -17,10 +17,16 @@ download_tar(){
   echo "  正在更新 $2..."
   curl -s -f $1/$2.tar.gz -o $2.tar.gz
   ERROR=$?
+  enable_git=`tar -tvf $2.tar.gz | grep ".git/config"`
   if [ "$ERROR" -eq "0" ]; then
     if [ "$4"x = "true"x ]; then
-      mkdir -p $2
-      tar -xf $2.tar.gz -C ./$2
+      if [ -z $enable_git ]; then
+        mkdir -p $2
+        tar -xf $2.tar.gz -C ./$2
+      else
+        echo " git化的资源，创建符号链接... "
+        ln_tar
+      fi
     fi
     echo "  $2 更新完毕"
   else
@@ -32,32 +38,17 @@ download_tar(){
   fi
 }
 
-download_ln_tar(){
-  # $1: url $2: filename $3: 是否忽略不存在的资源 
-  rm -rf $2.tar.gz
-  echo "  正在更新 $2..."
-  curl -s -f $1/$2.tar.gz -o $2.tar.gz
-  ERROR=$?
-  if [ "$ERROR" -eq "0" ]; then
-    src_dir=$JUSTEP_HOME/source
-    mkdir -p $src_dir
-    tar -xf $2.tar.gz -C $src_dir
-    run_dir=$JUSTEP_HOME/model
-    for sub_name in `find $src_dir -maxdepth 1 -mindepth 1 -type d -printf "%f\n"` ; do
-      mkdir -p $run_dir/$sub_name
-      retc=`find $src_dir/$sub_name -mindepth 1 -maxdepth 1`
-      if [ ! -z "$retc" ]; then
-        find $src_dir/$sub_name -mindepth 1 -maxdepth 1 | xargs ln -t $run_dir/$sub_name -fs
-      fi
-    done
-    echo "  $2 更新完毕"
-  else
-    if [ "$3"x = "true"x ]; then
-      echo "  $2 不存在，忽略更新"
-    else
-      error "  [$ERROR]更新 $2 失败" 1
+# link source的子目录到model目录下
+ln_tar(){
+  src_dir=$JUSTEP_HOME/source
+  run_dir=$JUSTEP_HOME/model
+  for sub_name in `find $src_dir -maxdepth 1 -mindepth 1 -type d -printf "%f\n"` ; do
+    mkdir -p $run_dir/$sub_name
+    retc=`find $src_dir/$sub_name -mindepth 1 -maxdepth 1`
+    if [ ! -z "$retc" ]; then
+      find $src_dir/$sub_name -mindepth 1 -maxdepth 1 | xargs ln -t $run_dir/$sub_name -fs
     fi
-  fi
+  done
 }
 
 download_webapps(){
